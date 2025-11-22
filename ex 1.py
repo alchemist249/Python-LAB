80315659fc33042010ae2b589ee8ed034fee2997
AMRIT CHETRI <amritchetri249@gmail.com>
2025-11-22 12:16:12 +0530

    Add files via upload

---
+++ ex 1.py
@@ -0,0 +1,14 @@
+student1 = {
+    "name": "amrit ch",
+    "roll_no": 1,
+    "department": "Computer Science",
+    "year": "3rd sem",
+    "marks": {
+        "python": 100,
+        "DSA": 100,
+        "MATHS": 100,
+    },
+    "contact": "amritch@example.com"
+    
+}
+print(student1["marks"])
